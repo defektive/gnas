6b2746c8cf4df365a06dee35b1d1bf88671cb532
defektive <defektive@users.noreply.github.com>
2025-12-04 07:23:00 -0700

    updates

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package main
 
-import "github.com/defektive/gnas/pkg/cmd"
-
-const (
-	shortUsage = "Console application example, with cobra commands/flags/completions generated from structs"
+import (
+	"github.com/analog-substance/util/cli/build_info"
+	"github.com/analog-substance/util/cli/updater/cobra_updater"
+	"github.com/defektive/gnas/pkg/cmd"
 )
 
+var version = "v0.0.0"
+var commit = "replace"
+
 func main() {
+	versionInfo := build_info.InitLoadedVersion(version, commit)
+	cmd.RootCmd.Version = versionInfo.String()
+	cobra_updater.AddToRootCmd(cmd.RootCmd, versionInfo)
 
 	cmd.Execute()
-
 }
